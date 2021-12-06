564f1b2aab1770725a7773217593d2085a79c18e
Nguyen Quoc Hoi <nguyenquochoi342000@gmail.com>
2021-12-06 10:57:15 +0700

    urlconfig update

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,4 +1,4 @@
-const baseUrl = "http://localhost:9000";
+const baseUrl = "https://flipkart-becked.herokuapp.com";
 
 export const api = `${baseUrl}/api`;
 export const generatePublicUrl = (fileName) => {
